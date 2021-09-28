d3469523f0d6f816369e5d61eecb0e1ac9b6c80b
Riccardo Nobili <48481589+rickynobili@users.noreply.github.com>
2021-09-28 19:01:56 +0200

    Delete Functions directory

--- Functions/displayobjnotexture.m
+++
@@ -1,9 +0,0 @@
-function displayobjnotexture (obj)
-figure()
-patch('vertices', obj.v, 'faces', obj.f.v, ...
-'FaceVertexCData', rand(8,1));
-shading interp
-axis square;
-axis equal;
-view([-37.5, -30]);
-end
